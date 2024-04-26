52aa612dfd80b10a1bd1e8ac1fd78dd2a92f4a9e
GG <geeonmn072020@gmail.com>
2024-04-26 14:53:58 +0100

    Folder structures

---
+++ UserAPI/UserAPI/UserAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAPIApp.swift
+//  UserAPI
+//
+//  Created by Geethanjali on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct UserAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
